1711534c2ae2af0d964d7b6710dda0382d820a30
omni23 <89713821+omni23@users.noreply.github.com>
2023-04-14 17:42:12 -0400

    Add read status toggling

--- scripts/script.js
+++ scripts/script.js
@@ -7,68 +7,106 @@ const darkenDiv = document.querySelector("#darken-div");
 const inputList = document.querySelectorAll("input");
 const myLibrary = [];
 
-function Book(name, author, pages) {
-    this.name = name;
-    this.author = author;
-    this.pages = pages;
+function Book(name, author, pages, read) {
+  this.name = name;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
 
-function createBook() {
-    /* use values from form */
-    return new Book(inputList[0].value, inputList[1].value, 
-    inputList[2].value);
+function createBook(userList) {
+  /* use values from form */
+  return new Book(
+    userList[0].value,
+    userList[1].value,
+    userList[2].value,
+    userList[3].checked
+  );
 }
 
-function updateUI(book) {
-    const bookCard = document.createElement("div");
-    bookCard.classList.add("card");
-    bookCard.dataset.indexNumber = myLibrary.length;
+function updateBookStatus(book) {
+  if (book.read) {
+    book.read = false;
+  } else {
+    book.read = true;
+  }
+}
 
-    const bookName = document.createElement("h3");
-    bookName.textContent = book.name;
+function deleteBook(userArray, index) {
+  userArray.splice(index, 1);
+}
 
-    const bookAuthor = document.createElement("p");
-    bookAuthor.textContent = book.author;
+Book.prototype.updateBookStatus = updateBookStatus;
 
-    const bookPages = document.createElement("p");
-    bookPages.textContent = book.pages;
+/* functions to handle UI */
 
-    const iconContainer = document.createElement("div")
+function updateCardStatus(book, status, icon) {
+  if (book.read) {
+    status.textContent = "Read";
+    icon.src = "./assets/images/check.svg";
+  } else {
+    status.textContent = "Unread";
+    icon.src = "./assets/images/book-open.svg";
+  }
+}
 
-    const deleteIcon = document.createElement("img");
-    deleteIcon.src = "./assets/images/delete.svg";
+function deleteCard(container, card, index) {
+  card.remove();
 
-    deleteIcon.addEventListener("click", () => {
-        const deletedIndex = bookCard.dataset.indexNumber;
-        
-        bookCard.remove();
-        myLibrary.splice(deletedIndex, 1);
+  /* shift indices down one for proceeding book
+    cards */
+  for (let i = index; i < container.children.length; i++) {
+    container.children[i].dataset.indexNumber -= 1;
+  }
+}
 
-        /* shift indices down one for proceeding book
-        cards */
-        for(let i = deletedIndex;
-            i < bookContainer.children.length;
-            i++) {
-                bookContainer.children[i].dataset.indexNumber -= 1;
-            }
-    })
+function createCard(book) {
+  const bookCard = document.createElement("div");
+  bookCard.classList.add("card");
+  bookCard.dataset.indexNumber = myLibrary.length;
 
-    iconContainer.append(deleteIcon);
+  const bookName = document.createElement("h3");
+  bookName.textContent = book.name;
 
-    bookCard.append(bookName, bookAuthor, bookPages,
-                    iconContainer);
+  const bookAuthor = document.createElement("p");
+  bookAuthor.textContent = book.author;
 
-    bookContainer.appendChild(bookCard);
+  const bookPages = document.createElement("p");
+  bookPages.textContent = book.pages;
+
+  const bookStatus = document.createElement("p");
+
+  const iconContainer = document.createElement("div");
+
+  const deleteIcon = document.createElement("img");
+  deleteIcon.src = "./assets/images/delete.svg";
+
+  const readIcon = document.createElement("img");
+  updateCardStatus(book, bookStatus, readIcon);
+
+  readIcon.addEventListener("click", () => {
+    updateBookStatus(book);
+    updateCardStatus(book, bookStatus, readIcon);
+  });
+
+  deleteIcon.addEventListener("click", () => {
+    deleteBook(myLibrary, bookCard.dataset.indexNumber);
+    deleteCard(bookContainer, bookCard, bookCard.dataset.indexNumber);
+  });
+
+  iconContainer.append(readIcon, deleteIcon);
+  bookCard.append(bookName, bookAuthor, bookPages, bookStatus, iconContainer);
+  bookContainer.appendChild(bookCard);
 }
 
 function openForm() {
-    bookForm.style.display = "flex";
-    darkenDiv.classList.add("darken");
+  bookForm.style.display = "flex";
+  darkenDiv.classList.add("darken");
 }
 
 function closeForm() {
-    bookForm.style.display = "none";
-    darkenDiv.classList.remove("darken");
+  bookForm.style.display = "none";
+  darkenDiv.classList.remove("darken");
 }
 
 openFormButton.addEventListener("click", openForm);
@@ -76,9 +114,9 @@ openFormButton.addEventListener("click", openForm);
 closeFormButton.addEventListener("click", closeForm);
 
 addBookButton.addEventListener("click", (e) => {
-    e.preventDefault();
-    const newBook = createBook();
-    updateUI(newBook);
-    myLibrary.push(newBook);
-    closeForm();
+  e.preventDefault();
+  const newBook = createBook(inputList);
+  createCard(newBook);
+  myLibrary.push(newBook);
+  closeForm();
 });
